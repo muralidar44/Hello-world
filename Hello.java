b5f237b5bca903d013165f20dcb5a45a193f0002
root <root@JenkinsVM1.tlkvekoeneguxpvbpxmbqvyzbg.bx.internal.cloudapp.net>
2019-07-23 05:55:08 +0000

    added hellow world program

---
+++ Hello.java
@@ -0,0 +1,27 @@
+class Hello{
+
+	
+
+	public static void main(String[] args){
+
+		
+
+		for(int i=1;i<=10;i++){
+
+			
+
+			System.out.println("Hello Murali ... "+i);
+
+			
+
+		}
+
+		
+
+	}
+
+
+
+
+
+}
